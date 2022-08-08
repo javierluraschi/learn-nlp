2914d474d554f9b5ba8bb6f4baa22c4ffca09ffa
Javier Luraschi <jluraschi@gmail.com>
2022-08-08 07:52:18 -0700

    contar palabras por espacios

--- 001_espacios.py
+++ 001_espacios.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 
 import urllib.request
+from os.path import exists
 
 libro_url = "https://www.gutenberg.org/cache/epub/54430/pg54430.txt"
+libro_archivo = "libro.txt"
 
 # descargar libro
-urllib.request.urlretrieve(libro_url, "libro.txt")
+if not exists(libro_archivo):
+    urllib.request.urlretrieve(libro_url, libro_archivo)
+
+with open(libro_archivo) as f:
+    libro = f.read()
+    
+len(libro.split())
