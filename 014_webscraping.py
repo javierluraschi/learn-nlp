7ba45ea54e5d314fde168d9be9703d9c582138f3
Javier Luraschi <jluraschi@gmail.com>
2022-10-24 07:47:46 -0700

    fix typo

--- 014_webscraping.py
+++ 014_webscraping.py
@@ -1,8 +1,16 @@
-// pip install lxml
+# pip install lxml
 
 import requests
 from lxml import html
+import pandas as pd
 
 books = 'https://www.gutenberg.org/ebooks/results/?author=&title=&subject=&lang=es&category=&locc=&filetype=rdf&submit_search=Search&pageno=1'
 contents = requests.get(books).text
 
+tree = html.fromstring(contents)
+
+authors = tree.xpath('//table[1]//td[3]//li[1]//text()')
+titles = tree.xpath('//table[1]//td[4]//text()')
+links = tree.xpath('//table[1]//td[4]//a[1]//@href')
+
+pd.DataFrame().assign(authors = authors, titles = titles, links = links)
